0995e0c48d5f124b1b7d277cd886c56f78e76a9b
Simon Jayawardena <simonjayawardena@hotmail.co.uk>
2023-09-25 11:30:54 +0000

    feat: add js to move through questions in sequence via css display property

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
-/** Event functions for clicks or conditions to move onto next screen
- * 'Play Game' button will bring up instructions on click
+/** EVENT FUNCTIONS FOR CLICKS OR CONDITIONS TO MOVE ONTO NEXT SCREEN OR QUESTION
+ * 'Play Game' button on intro screen will bring up instructions on click
  * 'Play Game' button on instructions screen will bring up game screen on click
- * Selecting the 10th answer on game screen will bring up final screen
+ * Selecting an answer on questions shown on the game screen will bring up the next question, and finally the end screen
  * 'Play Again' button will return to intro screen
  */
 function showInstructions() {
@@ -14,8 +14,61 @@ function showGameSreen() {
     document.getElementById("game-screen").style.display = "block";
 }
 
-// SECTION NEEDED HERE FOR ALL FUNCTIONS RELATING TO THE ACTUAL GAME 
-// THIS IS JUST A PLACEHOLDER FUNCTION TO TEST PAGE CYCLING
+function showQuestion2() {
+    document.getElementById("usa-question").style.display = "none";
+    document.getElementById("uk-question").style.display = "block";
+}
+
+function showQuestion3() {
+    document.getElementById("uk-question").style.display = "none";
+    document.getElementById("egypt-question").style.display = "block";
+}
+
+function showQuestion4() {
+    document.getElementById("egypt-question").style.display = "none";
+    document.getElementById("japan-question").style.display = "block";
+}
+
+function showQuestion5() {
+    document.getElementById("japan-question").style.display = "none";
+    document.getElementById("india-question").style.display = "block";
+}
+
+function showQuestion6() {
+    document.getElementById("india-question").style.display = "none";
+    document.getElementById("morocco-question").style.display = "block";
+}
+
+function showQuestion7() {
+    document.getElementById("morocco-question").style.display = "none";
+    document.getElementById("croatia-question").style.display = "block";
+}
+
+function showQuestion8() {
+    document.getElementById("croatia-question").style.display = "none";
+    document.getElementById("philippines-question").style.display = "block";
+}
+
+function showQuestion9() {
+    document.getElementById("philippines-question").style.display = "none";
+    document.getElementById("sri-lanka-question").style.display = "block";
+}
+
+function showQuestion10() {
+    document.getElementById("sri-lanka-question").style.display = "none";
+    document.getElementById("burkina-faso-question").style.display = "block";
+}
+
+function showQuestion11() {
+    document.getElementById("burkina-faso-question").style.display = "none";
+    document.getElementById("suriname-question").style.display = "block";
+}
+
+function showQuestion12() {
+    document.getElementById("suriname-question").style.display = "none";
+    document.getElementById("sudan-question").style.display = "block";
+}
+
 function showEndScreen() {
     document.getElementById("game-screen").style.display = "none";
     document.getElementById("end-screen").style.display = "block";
