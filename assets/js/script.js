2089572237ab4cdd5450156ba6e6ab7f288c3673
Simon Jayawardena <simonjayawardena@hotmail.co.uk>
2023-09-28 17:11:03 +0000

    style: define global variables in new script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,28 @@
 /** DEFINING GLOBAL VARIABLES TO BE USED IN FUNCTIONS */
 
+const quizLength = quizQuestions.length;
+let flagImage = document.getElementById("flag-image");
+let questionText = documment.getElementById("question-text");
+let answer1Text = document.getElementById("answer-1-text");
+let answer2Text = document.getElementById("answer-2-text");
+let answer3Text = document.getElementById("answer-3-text");
+let answer4Text = document.getElementById("answer-4-text");
+let questionNumber = document.getElementById("question-number");
+let scoreSoFar = document.getElementById ("score-so-far");
+//STILL NEED TO ADD THEM FOR END SCREEN
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
